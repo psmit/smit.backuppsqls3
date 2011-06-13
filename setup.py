65d884a1a4b599388baeea654890c832073f495f
Peter Smit <peter@cis.hut.fi>
2011-06-13 10:38:59 +0300

    Changed version management

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+import sys
+
+sys.path = ['./src'] + sys.path
+
+from smit.backuppsqls3 import __version__
 
 setup(name='smit.backuppsqls3',
-      version='0.0.1dev',
+      version=__version__,
       description='Tool backupping and restoring psql to s3',
       author='Peter Smit',
       author_email='peter@smitmail.eu',
@@ -11,6 +16,9 @@ setup(name='smit.backuppsqls3',
       package_dir={'': 'src'},
       install_requires=[],
       entry_points = dict(console_scripts=[
-        'bckp = smit.backuppsqls3.bckp:run',
+        'pg_backup_s3 = smit.backuppsqls3.bckp:run',
+        'pg_backup_wal_s3 = smit.backuppsqls3.bckp:run',
+        'pg_restore_s3 = smit.backuppsqls3.bckp:run',
+        'pg_restore_wal_s3 = smit.backuppsqls3.bckp:run'
         ])
       )
